26c2d19420bbf50ded0389dac18b5747aa410af9
黄鹏飞 <641655770@qq.com>
2019-05-06 12:19:57 +0800

    增加企业入驻

---
+++ src/main/java/com/springboot/framework/dao/entity/Enterprise.java
@@ -0,0 +1,145 @@
+package com.springboot.framework.dao.entity;
+
+import java.util.Date;
+
+public class Enterprise {
+    private Integer id;
+
+    private Integer parkId;
+
+    private String name;
+
+    private String legalPerson;
+
+    private String contact;
+
+    private String businessLicense;
+
+    private String location;
+
+    private Date beginDate;
+
+    private Integer duration;
+
+    private String createBy;
+
+    private Date createDate;
+
+    private String updateBy;
+
+    private Date updateDate;
+
+    private Byte status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getParkId() {
+        return parkId;
+    }
+
+    public void setParkId(Integer parkId) {
+        this.parkId = parkId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getLegalPerson() {
+        return legalPerson;
+    }
+
+    public void setLegalPerson(String legalPerson) {
+        this.legalPerson = legalPerson == null ? null : legalPerson.trim();
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact == null ? null : contact.trim();
+    }
+
+    public String getBusinessLicense() {
+        return businessLicense;
+    }
+
+    public void setBusinessLicense(String businessLicense) {
+        this.businessLicense = businessLicense == null ? null : businessLicense.trim();
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location == null ? null : location.trim();
+    }
+
+    public Date getBeginDate() {
+        return beginDate;
+    }
+
+    public void setBeginDate(Date beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy == null ? null : createBy.trim();
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy == null ? null : updateBy.trim();
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/com/springboot/framework/dao/mapper/EnterpriseMapper.java
@@ -0,0 +1,17 @@
+package com.springboot.framework.dao.mapper;
+
+import com.springboot.framework.dao.entity.Enterprise;
+
+public interface EnterpriseMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Enterprise record);
+
+    int insertSelective(Enterprise record);
+
+    Enterprise selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Enterprise record);
+
+    int updateByPrimaryKey(Enterprise record);
+}
