8c5c35335b8812949c6f2e823f741d8f97bd1285
黄鹏飞 <641655770@qq.com>
2019-05-29 19:38:44 +0800

    新增电话宽带服务

---
+++ src/main/java/com/springboot/framework/dao/entity/ServeApply.java
@@ -0,0 +1,59 @@
+package com.springboot.framework.dao.entity;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Table(name = "tb_serve_apply")
+public class ServeApply {
+    @Id
+    private Integer id;
+
+    private String contact;
+
+    private String name;
+
+    private String address;
+
+    private Date createDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact == null ? null : contact.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+}
---
+++ src/main/java/com/springboot/framework/dao/mapper/ServeApplyMapper.java
@@ -0,0 +1,7 @@
+package com.springboot.framework.dao.mapper;
+
+import com.springboot.framework.dao.entity.ServeApply;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface ServeApplyMapper extends Mapper<ServeApply> {
+}
